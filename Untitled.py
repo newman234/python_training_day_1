e3885168b479524a2f058f02c1c3da63d12cac66
newman234 <85755470+newman234@users.noreply.github.com>
2021-06-29 11:07:54 -0700

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,388 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("hello")
+
+
+# In[2]:
+
+
+2+3
+
+
+# In[3]:
+
+
+2-3
+
+
+# In[4]:
+
+
+2*3
+
+
+# In[5]:
+
+
+2/3
+
+
+# In[6]:
+
+
+2+10*10+2
+
+
+# In[7]:
+
+
+2%4
+
+
+# variable assignment
+
+# In[8]:
+
+
+a=2
+
+
+# In[9]:
+
+
+a
+
+
+# In[10]:
+
+
+#variable name can't start with number
+
+
+# In[11]:
+
+
+#they can't conatain whitespace between them
+
+
+# In[12]:
+
+
+#they can't contain special character except underscore
+
+
+# In[13]:
+
+
+my_dog=2
+
+
+# In[14]:
+
+
+my_dog
+
+
+# In[15]:
+
+
+my_dog="new_bride"
+
+
+# In[16]:
+
+
+my_dog
+
+
+# In[17]:
+
+
+print(my_dog)
+
+
+# In[18]:
+
+
+a=5
+
+
+# In[19]:
+
+
+a=a+5
+
+
+# In[20]:
+
+
+a
+
+
+# In[21]:
+
+
+type(a)
+
+
+# In[22]:
+
+
+a=4.5
+
+
+# In[23]:
+
+
+type(a)
+
+
+# In[24]:
+
+
+#string in python
+
+
+# In[25]:
+
+
+"hello"
+
+
+# In[26]:
+
+
+"i' am good"
+
+
+# In[27]:
+
+
+len("hello world")
+
+
+# In[28]:
+
+
+print("hii\nworld")
+
+
+# In[29]:
+
+
+s="hello"
+
+
+# In[30]:
+
+
+s[0]
+
+
+# In[31]:
+
+
+print(s[0])
+
+
+# In[32]:
+
+
+#slicing and indexing in python
+
+
+# In[33]:
+
+
+my_string="hello"
+
+
+# In[34]:
+
+
+my_string
+
+
+# In[35]:
+
+
+my_string[0]
+
+
+# In[36]:
+
+
+my_string[3]
+
+
+# In[37]:
+
+
+my_string[2:4]
+
+
+# In[38]:
+
+
+my_string[::]
+
+
+# In[39]:
+
+
+my_string[:]
+
+
+# In[40]:
+
+
+my_string[::-1]
+
+
+# In[41]:
+
+
+#string properties and method
+
+
+# In[42]:
+
+
+s="hello world"
+
+
+# In[43]:
+
+
+s[1]='e'
+
+
+# In[44]:
+
+
+s+"we"
+
+
+# In[45]:
+
+
+s
+
+
+# In[46]:
+
+
+s.upper()
+
+
+# In[47]:
+
+
+s
+
+
+# In[48]:
+
+
+s.lower()
+
+
+# In[49]:
+
+
+a="avd"
+
+
+# In[50]:
+
+
+A="cd"
+
+
+# In[51]:
+
+
+a
+
+
+# In[52]:
+
+
+A
+
+
+# In[54]:
+
+
+#string are  immutable
+
+
+# In[55]:
+
+
+s="abcde"
+
+
+# In[56]:
+
+
+s.split("c")
+
+
+# In[57]:
+
+
+"a"=='a'
+
+
+# In[58]:
+
+
+#formating string in python
+
+
+# In[59]:
+
+
+name="jose"
+
+
+# In[60]:
+
+
+print("this is {}".format("rohan"))
+
+
+# In[61]:
+
+
+result=100/77
+
+
+# In[62]:
+
+
+result
+
+
+# In[64]:
+
+
+print("the value is {r}".format(r=result))
+
+
+# In[67]:
+
+
+print("the value is{r:3.2f}".format(r=result))
+
+
+# In[ ]:
+
+
+print("the value is{r:1.8f}".format(r=result))
+
